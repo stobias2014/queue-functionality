5beb617fb7dca556b07d3f51b05d10de1b4eb18e
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-14 18:04:15 -0500

    added functionality

--- src/com/tobias/saul/App.java
+++ src/com/tobias/saul/App.java
@@ -2,13 +2,37 @@ package com.tobias.saul;
 
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class App {
-	
+
 	public static void main(String[] args) {
 		Queue<String> orders = new PriorityQueue<String>();
+
+		orders.add("order1");
+		orders.add("order2");
+		orders.add("order3");
+		orders.add("order4");
+		orders.add("order5");
+		orders.add("order6");
 		
+		System.out.println("Current orders");
+		orders.stream().collect(Collectors.toList()).forEach(order -> System.out.println(order));
+		System.out.println();
 		
+		System.out.println("Peeking....");
+		System.out.println(orders.peek());
+		System.out.println();
+		
+		System.out.println("Polling...");
+		System.out.println(orders.poll());
+		System.out.println();
+		
+		System.out.println("Current Orders");
+		orders.stream().collect(Collectors.toList()).forEach(order -> System.out.println(order));
+		System.out.println();
+		
+		System.out.println("Size: " + orders.size());
 	}
 
 }
