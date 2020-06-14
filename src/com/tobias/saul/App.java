c81799d619108a8e6c2e01ea85975e5065d4876f
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-14 17:57:24 -0500

    init commit

---
+++ src/com/tobias/saul/App.java
@@ -0,0 +1,14 @@
+package com.tobias.saul;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class App {
+	
+	public static void main(String[] args) {
+		Queue<String> orders = new PriorityQueue<String>();
+		
+		
+	}
+
+}
